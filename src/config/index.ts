e9261ba284e574a388da8ee490bee6641b1cecee
RomainDoyen <romain.doyen97@gmail.com>
2024-09-02 10:29:44 +0400

    ci: Generate database schema and access Endpoint configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,18 @@
 import { Account, Client } from 'appwrite';
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-//    Configuration file for Appwrite SDK
+// Configuration du SDK Appwrite
 
-const client = new Client();
+const client: Client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+    .setEndpoint('https://cloud.appwrite.io/v1' as string)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID as string);
 
-export const account = new Account(client); 
+export const account: Account = new Account(client); 
+
+// Configuration du SDK Supabase
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey)
